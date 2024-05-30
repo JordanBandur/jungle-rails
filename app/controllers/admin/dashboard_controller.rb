f297e936fcf0df68b99d766064852edbe5e218e0
JordanBandur <jordanbandur@hotmail.ca>
2024-05-30 08:36:05 -0700

    Update admin dashboard to show counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
   include HttpAuthenticatable
   
   def show
+    @products_count = Product.count
+    @categories_count = Category.count
   end
 end
